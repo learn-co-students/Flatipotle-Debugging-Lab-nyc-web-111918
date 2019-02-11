04063f59103d0fa97256494b0451ccbe8090247e
Tonyjlum <tonyjlum@gmail.com>
2019-02-10 23:18:27 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react'
 import './App.css'
-import Form from './Form'
-import Order from './Order'
+import Form from './components/Form'
+import Order from './components/Order'
 
 class App extends Component {
-  state: {
+
+  state= {
     orders: []
   }
 
   addOrder = (order) => {
     this.setState({
-      orders: this.state.orders.concat(order)
+      orders: [...this.state.orders, order]
+    })
+  }
+
+  // orders = () => this.state.orders.map( (order, idx) => {
+  //   return <Order key={idx} orders={order} />
+  // })
+
+  orders = () => {
+    return this.state.orders.map( (order, idx) => {
+      return <Order key={idx} orders={order} />
     })
   }
 
   render() {
-    const orders = this.state.orders.map( (order, idx) => {
-      <Order key={idx} {...order} />
-    })
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={ require('./images/logo.png') } className="App-logo" alt="logo" />
         </header>
 
-        <Form />
+        <Form addOrder={this.addOrder}/>
 
         <div className="ui raised container segment">
           <h1 className="ui block header">All Orders</h1>
           <div className="ui three cards">
-            { orders }
+            {this.orders() }
           </div>
         </div>
       </div>
